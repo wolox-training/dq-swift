81dd8de19b0387e9cbc682127fdd09338ca81840
DiegoQuiros94 <diego.quiros@wolox.com.ar>
2018-03-26 18:04:29 -0300

    implemented gallery and camera options for adding images

--- WBooks/Views/AddNew/AddNewView.swift
+++ WBooks/Views/AddNew/AddNewView.swift
@@ -12,24 +12,25 @@ import Core
 public class AddNewView: UIView, NibLoadable {
     
     @IBOutlet weak var formContainer: UIView!
-    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var imageButton: UIView!
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var author: UITextField!
     @IBOutlet weak var year: UITextField!
     @IBOutlet weak var topic: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var descript: UITextField!
+    @IBOutlet weak var bookImage: UIImageView!
     
-//    func configureButton() {
-//        submitButton.blueGradient()
-//        submitButton.titleLabel?.text = "Submit"
-//        submitButton.titleLabel?.textColor = .white
-//    }
+    func configureButton() {
+        submitButton.blueGradient()
+        submitButton.titleLabel?.text = "Submit"
+        submitButton.titleLabel?.textColor = .white
+    }
     
     func configureView() {
         
         backgroundColor = UIColor(red: 0.92, green: 0.96, blue: 0.98, alpha: 1)
-
+        
         formContainer.layer.cornerRadius = 5
         formContainer.backgroundColor = UIColor.white
         formContainer.layer.shadowOffset = CGSize(width: 0, height: 1)
@@ -43,12 +44,13 @@ public class AddNewView: UIView, NibLoadable {
         topic.underlined()
         descript.underlined()
         
-        submitButton.titleLabel?.text = "SUBMIT"
+//        submitButton.titleLabel?.text = "SUBMIT"
         submitButton.contentEdgeInsets = UIEdgeInsets(top: 15, left: 0, bottom: 15, right: 0)
-        submitButton.layer.cornerRadius = 25
-        submitButton.blueGradient()
+//        submitButton.layer.cornerRadius = 25
+//        submitButton.blueGradient()
         
     }
+    
 }
 
 extension UITextField {
--- WBooks/Views/AddNew/AddNewViewController.swift
+++ WBooks/Views/AddNew/AddNewViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class AddNewViewController: UIViewController {
+class AddNewViewController: UIViewController, UIGestureRecognizerDelegate, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
     
     private lazy var _view: AddNewView = AddNewView.loadFromNib()!
 
@@ -17,7 +18,7 @@ class AddNewViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-//        _view.configureButton()
+        _view.configureButton()
     }
     
     override func viewDidLoad() {
@@ -26,6 +27,67 @@ class AddNewViewController: UIViewController {
         
         _view.configureView()
         
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+        
+        _view.imageButton.addGestureRecognizer(tap)
+        
+        _view.imageButton.isUserInteractionEnabled = true
+        
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        let alert = UIAlertController(title: "select source", message: "",
+                                      preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { action in
+            switch action.style {
+            case .default:
+                if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                    let imagePicker = UIImagePickerController()
+                    imagePicker.delegate = self
+                    imagePicker.sourceType = .photoLibrary;
+                    imagePicker.allowsEditing = true
+                    self.present(imagePicker, animated: true, completion: nil)
+                }
+                
+            case .cancel:
+                break
+                
+            case .destructive:
+                break
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { action in
+            switch action.style {
+            case .default:
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    var imagePicker = UIImagePickerController()
+                    imagePicker.delegate = self
+                    imagePicker.sourceType = .camera;
+                    imagePicker.allowsEditing = false
+                    self.present(imagePicker, animated: true, completion: nil)
+                }
+            case .cancel:
+                break
+                
+            case .destructive:
+                break
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
+//        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+//            var imagePicker = UIImagePickerController()
+//            imagePicker.delegate = self
+//            imagePicker.sourceType = .photoLibrary;
+//            imagePicker.allowsEditing = true
+//            self.present(imagePicker, animated: true, completion: nil)
+//        }
+        
+    }
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        //swiftlint:disable:next force_cast
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        _view.bookImage.image = image
+        dismiss(animated:true, completion: nil)
     }
 
 }
